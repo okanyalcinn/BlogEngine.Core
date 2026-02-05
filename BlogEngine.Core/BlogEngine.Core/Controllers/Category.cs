e9cf1ed5a3608c86f6790410f12375312c5e5245
Okan Yalçın <yalcinokann@gmail.com>
2026-02-05 19:31:01 +0300

    20260205

---
+++ BlogEngine.Core/BlogEngine.Core/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogEngine.Core.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BlogEngine.Core/BlogEngine.Core/Controllers/HomeController.cs
+++ BlogEngine.Core/BlogEngine.Core/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace BlogEngine.Core.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
