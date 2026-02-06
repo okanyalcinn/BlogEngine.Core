5025f66004332e0384f32826c270e67394932d5e
Okan Yalçın <yalcinokann@gmail.com>
2026-02-06 20:21:35 +0300

    20250206

---
+++ BlogEngine.Core/BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    internal interface IBlogService
+    {
+        void AddCategory(Blog blog);
+        void DeleteCategory(Blog blog);
+        void UpdateCategory(Blog blog);
+        List<Blog> GetList();
+        Blog GetById(int id);
+        List<Blog> GetBlogListWithCategory();
+    }
+}
